bb2610b045961cdaa2761bf815f6ac33d6e1ccc2
Phosfis <paul.francois@isen.yncrea.fr>
2026-03-09 09:49:11 +0100

    added login screen

--- app/src/main/java/fr/isen/lucasribeiro/waltfiles/MainActivity.kt
+++ app/src/main/java/fr/isen/lucasribeiro/waltfiles/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import fr.isen.lucasribeiro.waltfiles.ui.LoginScreen
 import fr.isen.lucasribeiro.waltfiles.ui.theme.WaltFilesTheme
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +21,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WaltFilesTheme {
+                var isLoggedIn by remember { mutableStateOf(FirebaseAuth.getInstance().currentUser != null) }
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    if (isLoggedIn) {
+                        Greeting(
+                            name = FirebaseAuth.getInstance().currentUser?.email ?: "User",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    } else {
+                        Box(modifier = Modifier.padding(innerPadding)) {
+                            LoginScreen(onLoginSuccess = {
+                                isLoggedIn = true
+                            })
+                        }
+                    }
                 }
             }
         }
@@ -36,12 +48,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WaltFilesTheme {
-        Greeting("Android")
-    }
 }
